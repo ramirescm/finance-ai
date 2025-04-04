184d091c8f99edbe39c0dea9772c02d72c28b102
Ramires <ramirescm@gmail.com>
2025-04-04 20:35:26 -0300

    chore: add tailwind prettier plugin

--- app/transactions/[id]/page.tsx
+++ app/transactions/[id]/page.tsx
@@ -1,5 +1,5 @@
 const Transactions = ({ params: { id } }) => {
-  return <h1>Transactions: {id}</h1>;
+  return <h1 className="p-5 text-red-500">Transactions: {id}</h1>;
 };
 
 export default Transactions;
