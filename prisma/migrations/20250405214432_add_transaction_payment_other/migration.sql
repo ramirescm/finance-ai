4c059001ce7f30daab9d4da172b9bb4a3ae8c301
Ramires <ramirescm@gmail.com>
2025-04-09 22:17:12 -0300

    fix: migrations

--- prisma/migrations/20250405214432_add_transaction_payment_other/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "TransactionPaymentMethod" ADD VALUE 'OTHER';
--- prisma/migrations/20250410011326_init_db/migration.sql
+++ prisma/migrations/20250410011326_init_db/migration.sql
@@ -5,7 +5,7 @@ CREATE TYPE "TransactionType" AS ENUM ('DEPOSIT', 'EXPENSE', 'INVESTMENT');
 CREATE TYPE "TransactionCategory" AS ENUM ('HOUSING', 'TRANSPORTATION', 'FOOD', 'ENTERTAINMENT', 'HEALTH', 'UTILITY', 'SALARY', 'EDUCATION', 'OTHER');
 
 -- CreateEnum
-CREATE TYPE "TransactionPaymentMethod" AS ENUM ('CREDIT_CARD', 'DEBIT_CARD', 'BANK_TRANSFER', 'BANK_SLIP', 'CASH', 'PIX');
+CREATE TYPE "TransactionPaymentMethod" AS ENUM ('CREDIT_CARD', 'DEBIT_CARD', 'BANK_TRANSFER', 'BANK_SLIP', 'CASH', 'PIX', 'OTHER');
 
 -- CreateTable
 CREATE TABLE "Transaction" (
@@ -18,6 +18,7 @@ CREATE TABLE "Transaction" (
     "date" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
 );
